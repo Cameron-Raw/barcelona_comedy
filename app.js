5d805121d54d4b6a82b9c4c343c2670bd9d9c3c8
Cameron Raw <craw@sellbytel.es>
2018-03-13 13:41:14 +0100

    Server now accesses JSON files

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const port = 3000;
 const express = require('express');
 const app = express();
 var path = require('path');
+var fs = require('fs');
+
+var comediansJSON = fs.readFileSync(path.join(__dirname + '/builds/js/comedians.json'), 'utf8');
+var eventsJSON = fs.readFileSync(path.join(__dirname + '/builds/js/events.json'), 'utf8');
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/builds/index.html')));
+app.get('/index.html', (req, res) => res.sendFile(path.join(__dirname + '/builds/index.html')));
+
+app.get('/comedians.html', (req, res) => res.sendFile(path.join(__dirname + '/builds/comedians.html')));
 
 app.listen(3000, function(){
 	console.log('Listening on port 3000!');
+	console.log(comediansJSON);
+	console.log(eventsJSON);
 });
